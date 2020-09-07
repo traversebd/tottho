4e8427b5c49cc58ace32357c43a103698d04594e
shakiz <activities335@gmail.com>
2020-09-08 01:02:40 +0600

    profile and main ui update

--- app/src/main/java/com/teamtraverse/tottho/adapters/RecyclerAdapterRecentPost.java
+++ app/src/main/java/com/teamtraverse/tottho/adapters/RecyclerAdapterRecentPost.java
@@ -4,13 +4,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.teamtraverse.tottho.R;
 import com.teamtraverse.tottho.models.link.Post;
-
 import java.util.ArrayList;
 
 public class RecyclerAdapterRecentPost extends RecyclerView.Adapter<RecyclerAdapterRecentPost.ViewHolder> {
