a88101ce3ba7060d284b08555c5eb0d08b3e8d10
shakiz <activities335@gmail.com>
2020-10-19 23:26:46 +0600

    icons fixed

--- app/src/main/java/com/teamtraverse/tottho/activities/scholarship/ScholarshipListActivity.java
+++ app/src/main/java/com/teamtraverse/tottho/activities/scholarship/ScholarshipListActivity.java
@@ -25,7 +25,7 @@ public class ScholarshipListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scholarship);
+        setContentView(R.layout.activity_scholarship_list);
 
         //region initUI and then perform all UI actions
         initUI();
